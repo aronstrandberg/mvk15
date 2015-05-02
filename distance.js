39f76b477628ff32e9e7ebc925bf4ec90c5ce898
Patstrom <Patstrom@users.noreply.github.com>
2015-05-02 11:55:00 +0200

    Create distance.js
    Calculates the distance between two GPS coordinates. lat and lon are in degrees. Source commented in code.

---
+++ distance.js
@@ -0,0 +1,16 @@
+function distance(lat1, lat2, lon1, lon2) {
+	//Source can be found at http://www.movable-type.co.uk/scripts/latlong.html
+
+	var R = 6371000; // metres
+	var φ1 = lat1.toRadians();
+	var φ2 = lat2.toRadians();
+	var Δφ = (lat2-lat1).toRadians();
+	var Δλ = (lon2-lon1).toRadians();
+
+	var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ/2) * Math.sin(Δλ/2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+	return R * c;
+}
