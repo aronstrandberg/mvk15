12d4424ffd4efc65fe28cacc9ff8be2964865d97
Felix De Silva <felixds@kth.se>
2015-05-07 11:52:25 +0200

    Create Database.sql

---
+++ SQL Database/Database.sql
@@ -0,0 +1,24 @@
+-- Table for keeping track of the laps
+CREATE TABLE laps(
+	id SERIAL PRIMARY KEY,
+	droneid INTEGER,
+)
+
+-- Table for keeping track of the GPS points
+CREATE TABLE Location(
+	id SERIAL PRIMARY KEY,
+	timestamp TEXT,
+	longitude FLOAT,
+	latitude FLOAT,
+	speed FLOAT,
+	altitude FLOAT,
+	lap INTEGER,
+	read BOOLEAN
+);
+
+-- Sample data
+INSERT INTO Location(timestamp, longitude, latitude, speed, altitude, lap, read) VALUES ('03-05-2015', 18.07354, 59.34733, 12.123, 1.1234, 1, False);
+INSERT INTO Location(timestamp, longitude, latitude, speed, altitude, lap, read) VALUES ('03-05-2015', 18.07292, 59.34714, 12.123, 1.1234, 1, False);
+INSERT INTO Location(timestamp, longitude, latitude, speed, altitude, lap, read) VALUES ('03-05-2015', 18.07300, 59.34699, 12.123, 1.1234, 1, False);
+INSERT INTO Location(timestamp, longitude, latitude, speed, altitude, lap, read) VALUES ('03-05-2015', 18.07341, 59.34702, 12.123, 1.1234, 1, False);
+INSERT INTO Location(timestamp, longitude, latitude, speed, altitude, lap, read) VALUES ('03-05-2015', 18.07358, 59.34711, 12.123, 1.1234, 1, False);
