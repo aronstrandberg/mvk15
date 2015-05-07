85c4aabed8b694e860c327f4b839e45a599fe676
Henry Eklind <henry@vaert.se>
2015-05-07 11:53:27 +0200

    Update lol.php
    Uses PDO which is more secure, optimized etc. Also better code formatting.

--- Prototype/points/lol.php
+++ Prototype/points/lol.php
@@ -1,44 +1,54 @@
 <?php
 
-// If we haven't supplied an id, we probably want all of them.
-$id = -1;
-// If we supply an id we have got all points up till that id.
-if(isset($_GET['id'])) {
-  $id = intval($_GET['id']);
-}
-
-// Magic number for the latest number.
-$latest = -1;
-
-// If we haven't supplied an lap, we probably want the latest.
-$lap = $latest;
-// If we supply an lap we have got all points up till that lap.
-if(isset($_GET['lap'])) {
-  $lap = intval($_GET['lap']);
-}
+// get_id and get_lap.
+require 'functions.php';
 
 // Set up connection to database.
 // Otherwise return HTTP response code 418.
-$con = pg_connect("host=137.135.248.194 port=5432 dbname=mvk user=azureuser password=password")
-  or die(http_response_code(418));
+try {
+  $db = new PDO(
+    'pgsql:dbname=mvk host=137.135.248.194 port=5432',
+    "azureuser",
+    "password"
+  );
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo 'Connection failed: ' . $e->getMessage();
+  die(http_response_code(418));
+}
+
+// If we haven't supplied an id, we probably want all of them.
+$id = -1;
+
+// Magic number for the latest lap.
+$latest = -1;
+// If we haven't supplied an lap, we probably want the latest.
+$lap = $latest;
+
+// Update id and lap if we got a query.
+get_id();
+get_lap();
+
+// Get the latest lap if we haven't specified a lap.
+if ($lap == $latest) {
+  $query = "select max(lap) from location;";
+  $max = $db->query($query)->fetch()["max"];
+  $lap = $max;
+}
 
 // Construct query.
-$query = "select * from location where (id > $id and lap = ";
-if ($lap == $latest) {
-  $query .= "(select max(lap) from location)";
-} else {
-  $query .= "$lap";
-}
-$query .= ");";
+$query = "select * from location where (id > :id and lap = :lap);";
 
-// Perform the SQL query!
-$result = pg_query($con, $query);
-if (!$result) {
-  exit;
-}
+// Prepare query.
+$stmt = $db->prepare($query);
+$stmt->execute(array(':id' => $id, ':lap' => $lap));
+
+// Fetch all results.
+$res = $stmt->fetchAll();
 
 // Num of tuples from the database.
-$num = pg_affected_rows($result);
+$num = count($res);
+
 // We haven't got any new tuples.
 if ($num == 0) {
   // Special response code, saying there's no more content.
@@ -54,11 +64,11 @@ $geoJson = <<< HERE
 HERE;
 
 // Convert tuples to GeoJSON.
-while ($row = pg_fetch_assoc($result)) {
+foreach($res as $tuple) {
   // When we recieve a zeroed tuple, the drone has finished the lap.
-  if ($row["timestamp"] == -1
-   && $row["altitude"]  == -1
-   && $row["speed"]     == -1) {
+  if ($tuple["timestamp"] == -1
+   && $tuple["altitude"]  == -1
+   && $tuple["speed"]     == -1) {
     http_response_code(205);
     break;
   }
@@ -69,16 +79,16 @@ while ($row = pg_fetch_assoc($result)) {
       "geometry": {
         "type": "Point",
         "coordinates": [
-          $row[longitude],
-          $row[latitude]
+          $tuple[longitude],
+          $tuple[latitude]
         ]
       },
       "properties": {
-        "timestamp": "$row[timestamp]",
-        "lap": $row[lap],
-        "altitude": $row[altitude],
-        "velocity": $row[speed],
-        "id":       $row[id]
+        "timestamp": "$tuple[timestamp]",
+        "lap":        $tuple[lap],
+        "altitude":   $tuple[altitude],
+        "velocity":   $tuple[speed],
+        "id":         $tuple[id]
       }
     },
 HERE;
